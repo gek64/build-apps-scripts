6d98b5b37a3ce6bf1772a3b1cb212ab7678ef74b
gek64 <10346089+gek64@users.noreply.github.com>
2021-10-15 20:14:00 +0800

    Add files via upload

--- git_del_tag.sh
+++ git_del_tag.sh
@@ -2,16 +2,16 @@
 
 tag=""
 
-function get_tag(){
-    tag=$(git describe --tags)
+function get_tag() {
+  tag=$(git describe --tags)
 }
 
-function del_tag(){
-    if [[ $tag != *"fatal"* ]]; then
-        git push --delete origin $tag
-    else
-        echo -e "\033[31m无tag信息\033[0m"
-    fi
+function del_tag() {
+  if [[ $tag != *"fatal"* ]]; then
+    git push --delete origin "$tag"
+  else
+    echo -e "\033[31m无tag信息\033[0m"
+  fi
 }
 
 get_tag
