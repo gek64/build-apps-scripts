27c16deff738ee6f4675464e3f9a8181d423a519
gek64 <10346089+gek64@users.noreply.github.com>
2022-09-11 23:39:59 +0800

    Add files via upload

---
+++ gcc_g++_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PROGRAM_NAME="gps-sdr-sim"
+
+function init() {
+  apt -y update
+  apt -y install build-essential
+
+  # windows x86 x64
+  apt -y install binutils-mingw-w64
+  apt -y install gcc-mingw-w64 g++-mingw-w64
+  apt -y install gcc-mingw-w64-i686 g++-mingw-w64-i686
+  x86_64-w64-mingw32-gcc -v
+  x86_64-w64-mingw32-g++ -v
+  i686-w64-mingw32-gcc -v
+  i686-w64-mingw32-g++ -v
+
+  # linux arm64
+  apt -y install binutils-aarch64-linux-gnu
+  apt -y install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+  aarch64-linux-gnu-gcc -v
+  aarch64-linux-gnu-g++ -v
+
+  # linux x86 x64
+  apt -y install binutils-i686-linux-gnu binutils-x86-64-linux-gnu
+  apt -y install gcc-i686-linux-gnu g++-i686-linux-gnu
+  apt -y install gcc g++
+  i686-linux-gnu-gcc -v
+  i686-linux-gnu-g++ -v
+  x86_64-linux-gnu-gcc -v
+  x86_64-linux-gnu-g++ -v
+}
+
+function build() {
+  # linux
+  x86_64-linux-gnu-gcc gpssim.c -lm -O3 -o bin/${PROGRAM_NAME}-linux-x86_64
+  i686-linux-gnu-gcc gpssim.c -lm -O3 -o bin/${PROGRAM_NAME}-linux-x86
+  aarch64-linux-gnu-gcc gpssim.c -lm -O3 -o bin/${PROGRAM_NAME}-linux-arm64
+  # windows
+  x86_64-w64-mingw32-gcc gpssim.c -lm -O3 -o bin/${PROGRAM_NAME}-windows-x86_64
+  i686-w64-mingw32-gcc gpssim.c -lm -O3 -o bin/${PROGRAM_NAME}-windows-x86
+}
+
+init
+build
